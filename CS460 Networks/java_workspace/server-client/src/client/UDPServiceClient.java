bce1a41eb2776c2747e1f7458848b52cf2235047
Kelvin Rodriguez <kr788@students.froot.nau.edu>
2015-11-17 19:25:01 -0700

    finished udp client

--- CS460 Networks/java_workspace/server-client/src/client/UDPServiceClient.java
+++ CS460 Networks/java_workspace/server-client/src/client/UDPServiceClient.java
@@ -2,25 +2,60 @@ package client;
 
 import java.io.*;
 import java.net.*;
+import java.util.Arrays;
 
-class UDPClient {
-
-	public static void main(String args[]) throws Exception {
-		BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
-		DatagramSocket clientSocket = new DatagramSocket();
-		InetAddress IPAddress = InetAddress.getByName("localhost");
+class UDPServiceClient {
+	public static final int DEFAULT_PORT = 9890;
+	
+	public static final char CMD_ECHO 	= 'e';
+	public static final char CMD_DNS 	= 'd';
+	public static final char CMD_TIME   = 't';
 		
-		byte[] sendData = new byte[1024];
-		byte[] receiveData = new byte[1024];
-		String sentence = inFromUser.readLine();
+	public static void main (String[] args){
+		if(args.length < 1){
+			System.out.println("Usage: [service] [Parameter] [port]");
+			System.exit(0);
+		}
 		
-		sendData = sentence.getBytes();
+		byte[] buffer = new byte[255];
+		DatagramPacket receivePacket = null;
+		DatagramSocket clientSocket  = null;
 		
-		DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
-		clientSocket.send(sendPacket);
+		if(args[0].equals("-d"))
+			buffer[0] = 'd';
+		else if (args[0].equals("-e")) 
+			buffer[0] = 'e';
+		else if (args[0].equals("-t")) 
+			buffer[0] = 't';
+		else {
+			System.out.println("Unknown Service value: " + args[0]);
+			System.exit(0);
+		}
 		
-		DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-		clientSocket.receive(receivePacket);
+		buffer[1] = ' ';
+		
+		if (args.length > 1)
+			for(int i = 0; i < args[1].length(); i++)
+				buffer[i+2] = (byte) args[1].charAt(i);
+		
+		System.out.println("Sending: " + new String(buffer));
+		try {
+			
+			// create UDP socket
+			clientSocket = new DatagramSocket();
+			InetAddress IPAddress = InetAddress.getByName("134.114.9.128");
+			
+			// send the data
+			DatagramPacket sendPacket = new DatagramPacket(buffer, buffer.length, IPAddress, DEFAULT_PORT);
+			clientSocket.send(sendPacket);
+			
+			// wait for server response
+			receivePacket = new DatagramPacket(buffer, buffer.length);
+			clientSocket.receive(receivePacket);
+		} catch (Exception e) {
+			System.out.println("Shit Happens....");
+			System.exit(-1);
+		}
 		
 		String modifiedSentence = new String(receivePacket.getData());
 		System.out.println("FROM SERVER:" + modifiedSentence);
--- CS460 Networks/java_workspace/server-client/src/server/UDPServiceServer.java
+++ CS460 Networks/java_workspace/server-client/src/server/UDPServiceServer.java
@@ -11,32 +11,26 @@ public class UDPServiceServer
 	
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		/*
-		if(args.length != 1)
-		{
-			System.out.println("Needs port number only");
-			System.exit(0);
-		}*/
 
-			int port = 9998;
-			DatagramSocket serverSocket = new DatagramSocket(port);
-		    byte[] receiveData = new byte[1024];             
-		    byte[] sendData = new byte[1024];       
-		    
-		    while(true)                
-		    {                   
-		        DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-		        serverSocket.receive(receivePacket);
-		        String sentence = new String( receivePacket.getData());
-		       	System.out.println("RECEIVED: " + sentence);
-		       	InetAddress IPAddress = receivePacket.getAddress();
-		       	int ClientPort = receivePacket.getPort();
-		       	String capitalizedSentence = sentence.toUpperCase();
-              	sendData = capitalizedSentence.getBytes();
-              	DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, ClientPort);
-		       	serverSocket.send(sendPacket);
-		       	
-		    }	
+		int port = 9998;
+		DatagramSocket serverSocket = new DatagramSocket(port);
+	    byte[] receiveData = new byte[1024];             
+	    byte[] sendData = new byte[1024];       
+	    
+	    while(true)                
+	    {                   
+	        DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+	        serverSocket.receive(receivePacket);
+	        String sentence = new String( receivePacket.getData());
+	       	System.out.println("RECEIVED: " + sentence);
+	       	InetAddress IPAddress = receivePacket.getAddress();
+	       	int ClientPort = receivePacket.getPort();
+	       	String capitalizedSentence = sentence.toUpperCase();
+          	sendData = capitalizedSentence.getBytes();
+          	DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, ClientPort);
+	       	serverSocket.send(sendPacket);
+	       	
+	    }	
 	
 		}
 		         
